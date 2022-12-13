582d0db4ad9242fae74f80bed0a85f3dbf19425f
Nick <nikmaunt@gmail.com>
2022-12-13 18:19:58 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState, KeyboardEvent, ChangeEvent} from 'react';
 import './App.css';
+import {v4} from "uuid";
+import randomColor from "randomcolor";
+import Draggable, {DraggableData} from 'react-draggable';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [item, setItem] = useState<any>('')
+    // @ts-ignore
+    const [items, setItems] = useState<any>(JSON.parse(localStorage.getItem('items')) || [])
+    let [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items))
+    }, [items])
+
+    const newItem = () => {
+        if (item.trim() !== '') {
+            const newItem = {
+                id: v4(),
+                item,
+                color: randomColor({
+                    luminosity: 'light',
+                }),
+                defaultPos: {
+                    x: 400,
+                    y: -400
+                }
+            }
+            setItems((item: any) => [...items, newItem])
+            setItem('')
+        } else {
+            setError('Title is required')
+            setItem('')
+        }
+    }
+
+
+    const deleteNote = (id: string) => {
+        setItems(items.filter((i: any) => i.id !== id))
+    }
+    const updatePosition = (data: DraggableData, index: number) => {
+        let newArray = [...items]
+        newArray[index].defaultPos = {x: data.x, y: data.y}
+        setItems(newArray)
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (e.keyCode === 13) {
+            newItem()
+        }
+    }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setItem(e.target.value)
+    }
+
+
+    return (
+        <div className="App">
+            <div className='wrapper'>
+                <input className={ error ? 'error' : 'input'}  onKeyDown={onKeyPressHandler} value={item} onChange={onChangeHandler}
+                       type="text" placeholder={error ? 'Title is required' : 'Enter something...'}/>
+                <button className='enter' onClick={newItem}> Enter</button>
+            </div>
+            {
+                items.map((item: any, index: any) => {
+                    return (
+                        <Draggable key={index}
+                                   defaultPosition={item.defaultPos}
+                                   onStop={(_, data) => {
+                                       updatePosition(data, index)
+                                   }}>
+                            <div className="todo_item" style={{backgroundColor: item.color}}>
+                                {`${item.item}`}
+                                <button className='delete' onClick={() => deleteNote(item.id)}>
+                                    X
+                                </button>
+                            </div>
+                        </Draggable>
+                    )
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
